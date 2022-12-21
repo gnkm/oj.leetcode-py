8840f9770196034fccd59bffc25bb9f92c8d6514
gnkm <genki.matsunaga@gmail.com>
2022-12-21 23:24:50 +0900

    Add tests directory

---
+++ leetcode/tests/problems/no_0841/test.py
@@ -0,0 +1,20 @@
+from leetcode.problems.no_0841.mycode_01 import Solution as SolutionMyCode01
+
+
+ROOMS_01 = [[1], [2], [3], []]
+ROOMS_02 = [[1, 3], [3, 0, 1], [2], [0]]
+ROOMS_03 = [[1], [2], [1], []]
+
+
+class TestMyCode:
+    def test_01(self):
+        solution = SolutionMyCode01()
+        assert solution.canVisitAllRooms(ROOMS_01) == True
+
+    def test_02(self):
+        solution = SolutionMyCode01()
+        assert solution.canVisitAllRooms(ROOMS_02) == False
+
+    def test_03(self):
+        solution = SolutionMyCode01()
+        assert solution.canVisitAllRooms(ROOMS_03) == False

0e0eb9103e68475caa39752bc630b9bd4079975d
gnkm <genki.matsunaga@gmail.com>
2022-12-23 17:43:23 +0900

    Add a template test code file

---
+++ templates/test.py
@@ -0,0 +1,18 @@
+from leetcode.problems.no_{ID}.mycode_01 import Solution as SolutionMyCode01
+from leetcode.problems.no_{ID}.mycode_01 import SolutionModelAnswer
+
+
+# Input examples here
+INPUT_01 = ''
+INPUT_02 = ''
+...
+
+
+class TestMyCode:
+    def test_01(self):
+        pass
+
+
+class TestModelAnswer:
+    def test_01(self):
+        pass

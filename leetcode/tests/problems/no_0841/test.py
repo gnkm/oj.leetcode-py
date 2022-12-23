3df6f038689bb274276b1ecc60dc1e0020657c14
gnkm <genki.matsunaga@gmail.com>
2022-12-23 10:05:46 +0900

    0841: Add model answer

--- leetcode/tests/problems/no_0841/test.py
+++ leetcode/tests/problems/no_0841/test.py
@@ -1,4 +1,5 @@
 from leetcode.problems.no_0841.mycode_01 import Solution as SolutionMyCode01
+from leetcode.problems.no_0841.mycode_01 import SolutionModelAnswer
 
 
 ROOMS_01 = [[1], [2], [3], []]
@@ -17,4 +18,18 @@ class TestMyCode:
 
     def test_03(self):
         solution = SolutionMyCode01()
+        assert solution.canVisitAllRooms(ROOMS_03) == False
+
+
+class TestModelAnswer:
+    def test_01(self):
+        solution = SolutionModelAnswer()
+        assert solution.canVisitAllRooms(ROOMS_01) == True
+
+    def test_02(self):
+        solution = SolutionModelAnswer()
+        assert solution.canVisitAllRooms(ROOMS_02) == False
+
+    def test_03(self):
+        solution = SolutionModelAnswer()
         assert solution.canVisitAllRooms(ROOMS_03) == False

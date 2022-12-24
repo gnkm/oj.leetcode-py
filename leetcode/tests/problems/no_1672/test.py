1cf3507d5b17ce0262593397c0ec0c880f831f52
gnkm <genki.matsunaga@gmail.com>
2022-12-25 08:55:22 +0900

    1672: Format with black

--- leetcode/tests/problems/no_1672/test.py
+++ leetcode/tests/problems/no_1672/test.py
@@ -4,18 +4,9 @@ from leetcode.problems.no_1672.mycode import SolutionModelAnswer
 
 # Input examples here
 CASES = [
-    {
-        "input": [[1,2,3],[3,2,1]],
-        "answer": 6
-    },
-    {
-        "input": [[1,5],[7,3],[3,5]],
-        "answer": 10
-    },
-    {
-        "input": [[2,8,7],[7,1,3],[1,9,5]],
-        "answer": 17
-    },
+    {"input": [[1, 2, 3], [3, 2, 1]], "answer": 6},
+    {"input": [[1, 5], [7, 3], [3, 5]], "answer": 10},
+    {"input": [[2, 8, 7], [7, 1, 3], [1, 9, 5]], "answer": 17},
 ]
 
 
@@ -24,22 +15,26 @@ class TestMyCode:
         case_num = 0
         solution = SolutionMyCode()
         assert (
-            solution.maximumWealth(CASES[case_num]["input"]) == CASES[case_num]["answer"]
+            solution.maximumWealth(CASES[case_num]["input"])
+            == CASES[case_num]["answer"]
         )
 
     def test_01(self):
         case_num = 1
         solution = SolutionMyCode()
         assert (
-            solution.maximumWealth(CASES[case_num]["input"]) == CASES[case_num]["answer"]
+            solution.maximumWealth(CASES[case_num]["input"])
+            == CASES[case_num]["answer"]
         )
 
     def test_02(self):
         case_num = 2
         solution = SolutionMyCode()
         assert (
-            solution.maximumWealth(CASES[case_num]["input"]) == CASES[case_num]["answer"]
+            solution.maximumWealth(CASES[case_num]["input"])
+            == CASES[case_num]["answer"]
         )
+
 
 class TestModelAnswer:
     def test_01(self):

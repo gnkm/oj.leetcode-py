b4e391a01ad24fe06d22e75d90e7a823ea2d7c71
gnkm <genki.matsunaga@gmail.com>
2022-12-25 08:31:14 +0900

    Add process to edit package name in test code file

--- templates/create_files.py
+++ templates/create_files.py
@@ -15,31 +15,41 @@ import sys
 import icecream
 
 
-TEMPLATE_ANSWER_CODE_FILE = "templates/mycode.py"
-TEMPLATE_TEST_CODE_FILE = "templates/test.py"
+TEMPLATE_DIR = "templates"
+ANSWER_CODE_FILE_NAME = "mycode.py"
+TEST_CODE_FILE_NAME = "test.py"
+TEMPLATE_ANSWER_CODE_FILE = f"{TEMPLATE_DIR}/{ANSWER_CODE_FILE_NAME}"
+TEMPLATE_TEST_CODE_FILE = f"{TEMPLATE_DIR}/{TEST_CODE_FILE_NAME}"
+
+
+debug = icecream.ic
 
 
 def main():
     problem_id = sys.argv[1]
+    problem_id_formatted = f"{problem_id:0>4}"
+
     # codes for answering
-    answer_codes_dir = f"leetcode/problems/no_{problem_id:0>4}"
+    answer_codes_dir = f"leetcode/problems/no_{problem_id_formatted}"
     os.makedirs(answer_codes_dir)
     with open(f"{answer_codes_dir}/__init__.py", "a"):
         pass
     shutil.copy(TEMPLATE_ANSWER_CODE_FILE, answer_codes_dir)
 
     # codes for testing
-    test_codes_dir = f"leetcode/tests/problems/no_{problem_id:0>4}"
+    test_codes_dir = f"leetcode/tests/problems/no_{problem_id_formatted}"
     os.makedirs(test_codes_dir)
     with open(f"{test_codes_dir}/__init__.py", "a"):
         pass
-    shutil.copy(TEMPLATE_TEST_CODE_FILE, test_codes_dir)
+
+    with open(TEMPLATE_TEST_CODE_FILE, "r") as f:
+        org_code = f.read()
+        new_code = org_code.replace('{ID}', problem_id_formatted)
+
+    with open(f"{test_codes_dir}/{TEST_CODE_FILE_NAME}", "a") as f:
+        f.write(new_code)
 
     sys.exit()
-
-
-def debug(v):
-    icecream.ic(v)
 
 
 if __name__ == "__main__":

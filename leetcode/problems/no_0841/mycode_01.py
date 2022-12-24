9dd42c205573fbb69d725371d39ea5eac9bd09da
gnkm <genki.matsunaga@gmail.com>
2022-12-24 14:03:03 +0900

    0841: Add general breadth first search code

--- leetcode/problems/no_0841/mycode_01.py
+++ leetcode/problems/no_0841/mycode_01.py
@@ -13,6 +13,7 @@ Given an array rooms where rooms[i] is the set of keys that you can obtain if yo
 difficulty: medium
 """
 
+import collections
 from typing import List
 
 
@@ -51,3 +52,24 @@ class SolutionModelAnswer:
                     seen[nei] = True # mark that we've entered the room
                     stack.append(nei) # add the key to the todo list
         return all(seen) # Return true iff we've visited every room
+
+
+def bfs(G):
+    """Sample of breadth first search.
+
+    cf. https://ja.wikipedia.org/wiki/%E5%B9%85%E5%84%AA%E5%85%88%E6%8E%A2%E7%B4%A2
+    """
+    V = [ v for v in G.nodes() ]
+    Q = collections.deque([V[0]])
+    searched = { v: False for v in G.nodes() }
+    searched[V[0]] = True
+    T = []
+    while len(Q) != 0:
+        v = Q.popleft()
+        edges = [ (v, i ) for (v, i) in G.edges(v) if not searched[i] ]
+        for edge in edges:
+             v, i = edge
+             Q.append(i)
+             T += [ (v, i) ]
+             searched[i] = True
+    return T

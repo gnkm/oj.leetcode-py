5e61e7970e2c9cf0db8203762e1a8db4be625f13
gnkm <genki.matsunaga@gmail.com>
2022-12-25 09:41:11 +0900

    Add skip mark to model answer test code

--- templates/test.py
+++ templates/test.py
@@ -1,3 +1,5 @@
+import pytest
+
 from leetcode.problems.no_{ID}.mycode import Solution as SolutionMyCode
 from leetcode.problems.no_{ID}.mycode import SolutionModelAnswer
 
@@ -14,13 +16,15 @@ class TestMyCode:
     def test_00(self):
         case_num = 0
         solution = SolutionMyCode()
-        # edit method name
+        # Edit method name
         assert solution.problem_function(CASES[case_num]["input"]) == CASES[case_num]["answer"]
 
 
 class TestModelAnswer:
+    # Delete following line if test model answer
+    @pytest.mark.skip
     def test_00(self):
         case_num = 0
         solution = SolutionMyCode()
-        # edit method name
+        # Edit method name
         assert solution.problem_function(CASES[case_num]["input"]) == CASES[case_num]["answer"]

88a2804a7c808a7fc1e102992fa08ce8ecdeb4d2
gnkm <genki.matsunaga@gmail.com>
2022-12-25 12:17:54 +0900

    Add code for defining `debug()` function

--- templates/mycode.py
+++ templates/mycode.py
@@ -22,3 +22,15 @@ class SolutionModelAnswer:
     {URL}
     """
     pass
+
+
+if __name__ == "__main__":
+    import pkg_resources
+    if any([str(i).startswith('icecream') for i in pkg_resources.working_set]):
+        import icecream
+        _debug = icecream.ic
+    else:
+        _debug = print
+
+    global debug
+    debug = _debug

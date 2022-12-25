6b92ba101ff4805439816245b9b66f1f2e3b9678
gnkm <genki.matsunaga@gmail.com>
2022-12-25 14:29:09 +0900

    Change template test code

--- templates/test.py
+++ templates/test.py
@@ -13,18 +13,21 @@ CASES = [
 
 
 class TestMyCode:
+    solution = SolutionMyCode()
+
     def test_00(self):
         case_num = 0
         solution = SolutionMyCode()
         # Edit method name
-        assert solution.problem_function(CASES[case_num]["input"]) == CASES[case_num]["answer"]
+        assert self.solution.problem_function(CASES[case_num]["input"]) == CASES[case_num]["answer"]
 
 
 class TestModelAnswer:
+    solution = SolutionModelAnswer()
+
     # Delete following line if test model answer
     @pytest.mark.skip
     def test_00(self):
         case_num = 0
-        solution = SolutionMyCode()
         # Edit method name
-        assert solution.problem_function(CASES[case_num]["input"]) == CASES[case_num]["answer"]
+        assert self.solution.problem_function(CASES[case_num]["input"]) == CASES[case_num]["answer"]

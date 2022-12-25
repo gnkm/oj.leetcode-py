0689f16765034d14a2a34d7866f64c24d0de6260
gnkm <genki.matsunaga@gmail.com>
2022-12-25 14:47:33 +0900

    Make it be able to specify problem type when scaffolding

--- templates/create_files.py
+++ templates/create_files.py
@@ -4,10 +4,15 @@ Usage:
     python create_files.py {ID}
 
 Example:
-    python create_files.py 1234
+    Make files in `problems` directory
+    python create_files.py 123
+
+    Make files in `contests` directory
+    python create_files.py 123 --problem_type contests
 
 """
 
+import argparse
 import os
 import shutil
 import sys
@@ -26,30 +31,51 @@ debug = icecream.ic
 
 
 def main():
-    problem_id = sys.argv[1]
+    args_from_cli = parse_args()
+    problem_id = args_from_cli.problem_id
+    problem_type = args_from_cli.problem_type
+
     problem_id_formatted = f"{problem_id:0>4}"
 
     # codes for answering
-    answer_codes_dir = f"leetcode/problems/no_{problem_id_formatted}"
+    answer_codes_dir = f"leetcode/{problem_type}/no_{problem_id_formatted}"
     os.makedirs(answer_codes_dir)
     with open(f"{answer_codes_dir}/__init__.py", "a"):
         pass
     shutil.copy(TEMPLATE_ANSWER_CODE_FILE, answer_codes_dir)
 
     # codes for testing
-    test_codes_dir = f"leetcode/tests/problems/no_{problem_id_formatted}"
+    test_codes_dir = f"leetcode/tests/{problem_type}/no_{problem_id_formatted}"
     os.makedirs(test_codes_dir)
     with open(f"{test_codes_dir}/__init__.py", "a"):
         pass
 
     with open(TEMPLATE_TEST_CODE_FILE, "r") as f:
         org_code = f.read()
-        new_code = org_code.replace('{ID}', problem_id_formatted)
+        new_code = org_code.replace("{ID}", problem_id_formatted)
 
     with open(f"{test_codes_dir}/{TEST_CODE_FILE_NAME}", "a") as f:
         f.write(new_code)
 
     sys.exit()
+
+
+def parse_args():
+    parser = argparse.ArgumentParser(
+        description="Create some files for solving a problem.",
+    )
+    parser.add_argument(
+        "problem_id",
+        help="Problem ID",
+    )
+    parser.add_argument(
+        "--problem_type",
+        choices=["problems", "contests"],
+        default="problems",
+    )
+    args = parser.parse_args()
+
+    return args
 
 
 if __name__ == "__main__":

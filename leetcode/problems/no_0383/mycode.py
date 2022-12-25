d9d9f86d3921286cd25a00e98e26c13b4a28ca97
gnkm <genki.matsunaga@gmail.com>
2022-12-25 12:52:56 +0900

    0383: Add my code

---
+++ leetcode/problems/no_0383/mycode.py
@@ -0,0 +1,47 @@
+"""383. Ransom Note
+
+URL: https://leetcode.com/problems/ransom-note/
+
+## Description
+
+Given two strings `ransomNote` and `magazine`, return `true` if `ransomNote` can be constructed by using the letters from `magazine` and `false` otherwise.
+Each letter in `magazine` can only be used once in `ransomNote`.
+
+difficulty: Easy
+"""
+
+from collections import Counter
+from typing import Dict, List, Union
+
+
+# my code.
+class Solution:
+    def canConstruct(self, ransomNote: str, magazine: str) -> bool:
+        counter_ransome_note = Counter(ransomNote)
+        counter_magazine = Counter(magazine)
+        return all([counter_magazine[character] >= counter_ransome_note[character] for character in set(counter_ransome_note.elements())])
+
+
+class SolutionModelAnswer:
+    """
+    {URL}
+    """
+    pass
+
+
+if __name__ == "__main__":
+    import pkg_resources
+    if any([str(i).startswith('icecream') for i in pkg_resources.working_set]):
+        import icecream
+        _debug = icecream.ic
+    else:
+        _debug = print
+
+    global debug
+    debug = _debug
+
+    debug(Counter("abcabc"))  # ic| Counter("abcabc"): Counter({'a': 2, 'b': 2, 'c': 2})
+    debug(Counter("abcabc")["a"])  # ic| Counter("abcabc")["a"]: 2
+    debug(len(Counter("abcabc")))  # ic| len(Counter("abcabc")): 3
+    debug(list(Counter("abcabc").elements()))  # ic| list(Counter("abcabc").elements()): ['a', 'a', 'b', 'b', 'c', 'c']
+    debug(set(Counter("abcabc").elements()))  # ic| set(Counter("abcabc").elements()): {'c', 'a', 'b'}
---
+++ leetcode/tests/problems/no_0383/test.py
@@ -0,0 +1,45 @@
+import pytest
+
+from leetcode.problems.no_0383.mycode import Solution as SolutionMyCode
+from leetcode.problems.no_0383.mycode import SolutionModelAnswer
+
+
+# Input examples here
+CASES = [
+    {"input": {"ransome_note": "a", "magazine": "b"}, "answer": False},
+    {"input": {"ransome_note": "aa", "magazine": "ab"}, "answer": False},
+    {"input": {"ransome_note": "aa", "magazine": "aab"}, "answer": True},
+]
+
+
+class TestMyCode:
+    def test_00(self):
+        case_num = 0
+        solution = SolutionMyCode()
+        ransome_note = CASES[case_num]["input"]["ransome_note"]
+        magazine = CASES[case_num]["input"]["magazine"]
+        assert solution.canConstruct(ransome_note, magazine) == CASES[case_num]["answer"]
+
+    def test_01(self):
+        case_num = 1
+        solution = SolutionMyCode()
+        ransome_note = CASES[case_num]["input"]["ransome_note"]
+        magazine = CASES[case_num]["input"]["magazine"]
+        assert solution.canConstruct(ransome_note, magazine) == CASES[case_num]["answer"]
+
+    def test_02(self):
+        case_num = 2
+        solution = SolutionMyCode()
+        ransome_note = CASES[case_num]["input"]["ransome_note"]
+        magazine = CASES[case_num]["input"]["magazine"]
+        assert solution.canConstruct(ransome_note, magazine) == CASES[case_num]["answer"]
+
+
+class TestModelAnswer:
+    @pytest.mark.skip
+    def test_00(self):
+        case_num = 0
+        solution = SolutionMyCode()
+        ransome_note = CASES[case_num]["input"]["ransome_note"]
+        magazine = CASES[case_num]["input"]["magazine"]
+        assert solution.canConstruct(ransome_note, magazine) == CASES[case_num]["answer"]

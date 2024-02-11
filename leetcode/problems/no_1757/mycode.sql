3f83ff53476416cd1d28b81d6d82fbaeac467008
gnkm <genki.matsunaga@gmail.com>
2024-02-11 11:01:58 +0900

    1757: Add my code

---
+++ leetcode/problems/no_1757/mycode.sql
@@ -0,0 +1,11 @@
+-- Recyclable and Low Fat Products - LeetCode
+-- https://leetcode.com/problems/recyclable-and-low-fat-products/description/?envType=study-plan-v2&envId=top-sql-50
+
+select
+    product_id
+from
+    Products
+where
+  low_fats = "Y"
+    and recyclable = "Y"
+;

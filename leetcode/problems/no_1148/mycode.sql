9b0ec2e49a30626f7bf6c5c56293a64f0d6664a8
gnkm <genki.matsunaga@gmail.com>
2024-02-12 16:51:13 +0900

    1148: Add a code

---
+++ leetcode/problems/no_1148/mycode.sql
@@ -0,0 +1,12 @@
+-- Article Views I - LeetCode
+-- https://leetcode.com/problems/article-views-i/description/?envType=study-plan-v2&envId=top-sql-50
+
+SELECT
+    DISTINCT(author_id) as id
+FROM
+    Views
+WHERE
+    author_id = viewer_id
+ORDER BY
+    id
+;

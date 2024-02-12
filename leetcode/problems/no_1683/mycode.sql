62c56741e3513c1926ed0e7f253761fe5a292382
gnkm <genki.matsunaga@gmail.com>
2024-02-12 20:35:32 +0900

    1683: Add a code

---
+++ leetcode/problems/no_1683/mycode.sql
@@ -0,0 +1,9 @@
+-- Invalid Tweets - LeetCode
+-- https://leetcode.com/problems/invalid-tweets/description/?envType=study-plan-v2&envId=top-sql-50
+SELECT
+    tweet_id
+FROM
+    Tweets
+WHERE
+    char_length(content) > 15
+;

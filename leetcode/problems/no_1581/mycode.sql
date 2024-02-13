a20c1afafc35a49d8c49a2b67b0c604377ef338d
gnkm <genki.matsunaga@gmail.com>
2024-02-13 23:24:28 +0900

    1581: Add a code

---
+++ leetcode/problems/no_1581/mycode.sql
@@ -0,0 +1,15 @@
+-- Customer Who Visited but Did Not Make Any Transactions - LeetCode
+-- https://leetcode.com/problems/customer-who-visited-but-did-not-make-any-transactions/description/?envType=study-plan-v2&envId=top-sql-50
+
+select
+    v.customer_id as customer_id
+    , count(v.visit_id) as count_no_trans
+from
+    Visits v
+    left join Transactions t
+    on v.visit_id = t.visit_id
+where
+    t.visit_id is null
+group by
+    v.customer_id
+;

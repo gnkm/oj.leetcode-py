ac1f0d320a2d145b40d1b6baa5afd84c76a9fdb6
gnkm <genki.matsunaga@gmail.com>
2024-02-27 08:55:05 +0900

    0013: Add an example answer

---
+++ leetcode/problems/no_0013/example_answer.py
@@ -0,0 +1,41 @@
+"""13. Roman to Integer - LeetCode
+https://leetcode.com/problems/roman-to-integer/description/
+
+difficulty: Easy
+"""
+
+import sys
+from typing import Dict, List, Union
+
+
+class Solution:
+    def romanToInt(self, s: str) -> int:
+        translations = {"I": 1, "V": 5, "X": 10, "L": 50, "C": 100, "D": 500, "M": 1000}
+        number = 0
+        s = s.replace("IV", "IIII").replace("IX", "VIIII")
+        s = s.replace("XL", "XXXX").replace("XC", "LXXXX")
+        s = s.replace("CD", "CCCC").replace("CM", "DCCCC")
+        for char in s:
+            number += translations[char]
+        return number
+
+
+class SolutionModelAnswer:
+    """
+    {URL}
+    """
+
+    pass
+
+
+def main():
+    solution = Solution()
+    assert solution.romanToInt("III") == 3
+    assert solution.romanToInt("LVIII") == 58
+    assert solution.romanToInt("MCMXCIV") == 1994
+    sys.exit()
+
+
+if __name__ == "__main__":
+    main()
+    main()
